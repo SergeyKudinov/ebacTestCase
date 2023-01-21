4b5130ed46d9370db50a69c81b7cde45ca97fcd8
SergeyKudinov <serezha.kudinov.1995@mail.ru>
2023-01-21 23:06:25 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="app">
+    <h1>Страница с постами</h1>
+    <my-input
+        :model-value="searchQuery"
+        placeholder="Поиск..."
+    />
+    <div class="app_btns">
+        <my-button
+            @click="showDialog"
+        >
+            Создать пост
+        </my-button>
+    </div>
+    <my-dialog v-model:show="dialogVisible">
+        <PostForm
+            @create="createPost"
+        />
+    </my-dialog>
+    <ItemList
+        :posts="sortedAndSearchedPosts"
+        @remove="removePost"
+        v-if="!isPostsLoading"
+    />
+    <div v-else>Идет загрузка...</div>
+</div>
+</template>
+
+<script>
+import ItemList from './components/ItemList.vue';
+    import PostForm from './components/PostForm.vue';
+    import axios from 'axios';
+    import MySelect from './components/UI/MySelect.vue';
+    import MyInput from './components/UI/MyInput.vue';
+    import MyButton from './components/UI/MyButton.vue';
+
+    export default {
+        components: {
+            MyInput,
+            MySelect,
+            MyButton,
+            ItemList,
+            PostForm,
+            MySelect
+        },
+        data() {
+            return{
+                posts: [],
+                dialogVisible: false,
+                isPostsLoading: false,
+                selectedSort: '',
+                searchQuery: '',
+                sortOptions: [
+                    {value: 'title', name: 'По названию'},
+                    {value: 'body', name: 'По содержимому'},
+                ]
+            }
+        },
+        methods: {
+            createPost(post) {
+                this.posts.push(post);
+                this.dialogVisible = false;
+            },
+            removePost(post) {
+                this.posts = this.posts.filter(p => p.id !== post.id)
+            },
+            showDialog() {
+                this.dialogVisible = true;
+            },
+            async fetchPosts() {
+                try {
+                    this.isPostsLoading = true;
+                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+                    this.posts = response.data;
+                } catch(e) {
+                    console.log(e);
+                } finally {
+                    this.isPostsLoading = false;
+                }
+            },
+        },
+        mounted() {
+            this.fetchPosts();
+        },
+        computed: {
+            sortedPosts() {
+                return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localCompare(post2[this.selectedSort]))
+            },
+            sortedAndSearchedPosts() {
+                return this.sortedPosts.filter(post => post.title.includes(this.searchQuery))
+            }
+        }
+    }
+</script>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  .app {
+    padding: 20px;
+  }
+  .app_btns {
+    margin: 15px 0;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,51 @@
+<template>
+    <div v-show="posts.length > 0">
+        <h3>Список постов</h3>
+        <transition-group name="post_list">
+            <PostItem
+                v-for="post in posts"
+                :post="post"
+                :key="post.id"
+                @remove="$emit('remove', post)"
+            />
+        </transition-group>
+    </div>
+    <h2
+        v-show="posts.length === 0"
+        style="color: red">
+        Список постов пуст
+    </h2>
+</template>
+
+<script>
+import PostItem from './PostItem.vue';
+
+    export default {
+        props: {
+            posts: {
+                type: Array,
+                required: true
+            }
+        },
+        components: { PostItem }
+}
+</script>
+
+<style>
+    .post_list-item {
+        display: inline-block;
+        margin-right: 10px;
+    }
+    .post_list-enter-active,
+    .post_list-leave-active {
+            transition: all 0.4s ease;
+    }
+    .post_list-enter-from,
+    .post_list-leave-to {
+            opacity: 0;
+            transform: translateX(130px);
+    }
+    .post_list-move {
+        transition: transform 0.4s ease;
+    }
+</style>
